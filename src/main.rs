120a8c3980716d4f813977bd0fdcd1ff447fe36e
Williamjacobsen <villi05.v.j@gmail.com>
2025-12-14 15:35:02 +0100

    started over

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,18 @@
 use std::env;
 use std::error::Error;
 
-mod api;
-mod state;
-mod ui;
-mod utils;
-mod websocket;
-
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let app_state = state::create_app_state();
+    run_app()?;
 
-    api::initialize()?;
+    Ok(())
+}
 
-    let (update_sender, update_receiver) = state::create_update_channel();
+fn run_app() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
 
-    let app_state_clone = app_state.clone();
-    std::thread::spawn({
-        let update_sender = update_sender.clone();
-        move || {
-            let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async {
-                if let Err(e) = websocket::websocket::connect(app_state_clone, update_sender).await
-                {
-                    eprintln!("WebSocket error: {}", e);
-                }
-            });
-        }
-    });
-
-    ui::run_app(app_state, update_receiver)?;
+    ui.run()?;
 
     Ok(())
 }
